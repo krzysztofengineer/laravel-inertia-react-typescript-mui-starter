1ef275ce4aefdca03e7bb475b9c1fc2348c78d4c
Krzysztof <krzysztof@engineer.com>
2023-02-22 00:20:06 +0100

    Fix dusk example test

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -5,11 +5,7 @@ use Laravel\Dusk\Browser;
 
 it('works', function () {
     $this->browse(function (Browser $browser) {
-        $projects = Project::factory(count: 10)->create();
-
         $browser->visit('/')
-            ->waitForText($projects[0]->name)
-            ->assertSee($projects[0]->name)
-            ->assertSee($projects[9]->name);
+            ->assertSee('Paperbase');
     });
 });
